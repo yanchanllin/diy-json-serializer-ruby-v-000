330d3ceb0c54561760670d91fb0ddb6f2a57690b
YanC Lin <yonalin1@gmail.com>
2018-09-28 18:47:58 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,6 +29,7 @@ class PostsController < ApplicationController
   def post_data
     post = Post.find(params[:id])
     render json: PostSerializer.serialize(post)
+  end 
 
   def body
      post = Post.find(params[:id])
